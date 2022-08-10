080081677904419e67bc84638b3076c553982dea
Prathap <prathap.sims@gmail.com>
2022-08-09 19:38:49 -0700

    Add props name,stories for DayListItem and style for DayListItem

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import "components/DayListItem.scss";
+const classNames = require('classnames');
 
 export default function DayListItem(props) {
+  let dayClass = classNames('day-list__item', {
+    'day_list__item--selected': props.selected,
+    'day-list__item--full': !props.spots
+  })
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li
+    className={dayClass}
+    onClick={() => props.setDay(props.name)}
+    >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{props.spots}</h3>
     </li>
   );
 }
