71bbee2a18b31ed7d26e0be51117c9750630f30f
Prathap <prathap.sims@gmail.com>
2022-08-09 21:39:12 -0700

    Add DayList component and stories(index.js)

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -2,18 +2,37 @@ import React from "react";
 import "components/DayListItem.scss";
 const classNames = require('classnames');
 
+
+
+const formatSpots = spots => {
+  if (!spots) {
+    return `no spots remaining`;
+  }
+
+  if (spots === 1) {
+    return `${spots} spot remaining`;
+  }
+
+  return `${spots} spots remaining`;
+}
+
 export default function DayListItem(props) {
+  const availabilityMessage = formatSpots(props.spots);
+
+
   let dayClass = classNames('day-list__item', {
-    'day_list__item--selected': props.selected,
+    'day-list__item--selected': props.selected,
     'day-list__item--full': !props.spots
   })
+
+
   return (
     <li
     className={dayClass}
     onClick={() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{availabilityMessage}</h3>
     </li>
   );
 }
