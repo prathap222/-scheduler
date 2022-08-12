3b4c186b07b12d30794c335b1fadecc123866b09
Prathap <prathap.sims@gmail.com>
2022-08-12 01:22:17 -0700

    change useState and set functions to match combined useState

--- src/components/Application.js
+++ src/components/Application.js
@@ -53,13 +53,20 @@ const appointment = appointments.map((appt) => {
 
 
 export default function Application(props) {
-  const [day, setDay] = useState([]);
-  const[dayData, setDayData] = useState([]);
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  })
+
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get('http://localhost:8001/api/days')
     .then(res => {
-      setDayData(res.data)
+      setDays(res.data)
     })
     .catch(err => console.log(err))
   }, [])
@@ -75,8 +82,8 @@ export default function Application(props) {
           <hr className="sidebar__separator sidebar--centered" />
           <nav className="sidebar__menu">
           <DayList
-          days={dayData}
-          value={day}
+          days={state.days}
+          value={state.day}
           onChange={setDay}
         /> 
           </nav>
