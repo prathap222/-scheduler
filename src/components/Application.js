141c6fcf0a96958bf0308a59e07f78c95c9d370f
Prathap <prathap.sims@gmail.com>
2022-08-12 21:04:58 -0700

    fixed appointment retriving

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,7 +7,7 @@ import InterviewerList from "./InterviewerList";
 import Appointment from "components/Appointment";
 import axios from 'axios';
 
-import { matchAppointments, getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay } from "helpers/selectors";
 
 
 export default function Application(props) {
@@ -18,9 +18,10 @@ export default function Application(props) {
     appointments: {}
   })
 
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
   const setDay = day => setState({...state, day});
   
-
   useEffect(() => {
     Promise.all([
       Promise.resolve(axios.get('http://localhost:8001/api/days')),
@@ -29,14 +30,6 @@ export default function Application(props) {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
     })
   }, [])
-
-  const appointmentObjects = getAppointmentsForDay(state, state.day)
-
-  const appointment = appointmentObjects.map((appointmentObject) => {
-    return (
-        <Appointment key={appointmentObject.id} {...appointmentObject} />
-      )
-  })
 
   return (
     <main className="layout">
@@ -61,8 +54,12 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-        {appointment} 
-        <Appointment key="last" time="5pm" />
+        {dailyAppointments.map((appointmentObject) => {
+          return (
+              <Appointment key={appointmentObject.id} {...appointmentObject} />
+            )
+        })}
+        {<Appointment key="last" time="5pm" />}
       </section>
     </main>
   );
--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -16,8 +16,9 @@ export default function DayList(props) {
         key={day.id}
         name={day.name} 
         spots={day.spots}
-        selected={day.name === props.value}
+        selected={day.name === props.day}
         setDay={()=> props.onChange(day.name)}
+        
       />
       })}
 
