4aa45b32e2a9142deb07bceea254348a4d6482d8
Prathap <prathap.sims@gmail.com>
2022-08-12 23:44:40 -0700

    Retrieving interviewer data

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,18 +1,30 @@
-const matchAppointments = (appointments, ids) => {
+const matchIds = (appointments, ids) => {
   const matched = ids.map(id => appointments[id]);
   return matched;
 }
 
-//Go through a state array with a days object and an appointments object
-//Match the appointments given in the days object to those in the appointments object
+
 function getAppointmentsForDay(state, day) {
+
   let appointmentArr = [];
   state.days.map(dayObject => {
     if (dayObject.name === day) {
       dayObject.appointments.forEach(apptId => appointmentArr.push(apptId))
     }
   })
-  return matchAppointments(state.appointments, appointmentArr);
+  return matchIds(state.appointments, appointmentArr);
 }
 
-module.exports = { matchAppointments, getAppointmentsForDay };
+function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+
+  const interviewerInfo = state.interviewers[interview.interviewer];
+  return {
+    student: interview.student,
+    interviewer: interviewerInfo
+  }
+}
+
+module.exports = {matchIds, getAppointmentsForDay, getInterview };
